589e6884b165ce18667190a00993b4b82bbd98ee
Jojo <jojo.schultz@outlook.de>
2021-01-10 21:29:51 +1000

    added summary data

--- forceAnalysis/cli.py
+++ forceAnalysis/cli.py
@@ -19,8 +19,8 @@ def main(ctx, verbose):
 ##################################################################################
 
 @main.command(context_settings=CONTEXT_SETTINGS)
-@click.argument('subject')
-@click.option('overwrite_csv_files',
+@click.option('subject', help='subject which to assemble data for: either "magneto" or "lizards"')
+@click.argument('overwrite_csv_files',
               default=True,
               help='determines if csv files should be created and saved. If True, plots that exist already will be overwritten.')
 @click.pass_context
@@ -30,12 +30,13 @@ def assemble_force_data(_, *args, **kwargs):
     """Reads in the given list of .csv files for the subject and assembled the data.
     Arguments \n
     ---------- \n
-    subject : string \n
-    \tString either "lizards" or "magneto" \n
-    Options \n
-    ---------- \n
     overwrite_csv_files: boolean \n
     \tBoolean either True or False. Default: True. determine if to overwrite the csv data files for every run
+
+    Options \n
+    ---------- \n
+    subject : string \n
+    \tString either "lizards" or "magneto" \n
     """
     from forceAnalysis.operations import assemble_data
     assemble_data.assemble(*args, **kwargs)
@@ -45,7 +46,7 @@ def assemble_force_data(_, *args, **kwargs):
 ##################################################################################
 
 @main.command(context_settings=CONTEXT_SETTINGS)
-@click.option('overwrite_plots',
+@click.argument('overwrite_plots',
               default=True,
               help='determines if plots should be created and saved. If True, plots that exist already will be overwritten.')
 @click.pass_context
@@ -66,11 +67,10 @@ def plot_forces(_, *args, **kwargs):
 ##################################################################################
 
 @main.command(context_settings=CONTEXT_SETTINGS)
-@click.option('overwrite_plots',
+@click.argument('overwrite_plots',
               default=True,
               help='determines if plots should be created and saved. If True, plots that exist already will be overwritten.')
 @click.pass_context
-
 
 def plot_imu(_, *args, **kwargs):
     """Reads in the list of assembled.csv files and plots the force data
