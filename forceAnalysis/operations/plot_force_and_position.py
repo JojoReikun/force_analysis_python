094fcdb01522c85197757d47049a1f7842cfeadf
Jojo <jojo.schultz@outlook.de>
2021-01-13 11:04:30 +1000

    repaired force and position plot again

--- forceAnalysis/operations/plot_force_and_position.py
+++ forceAnalysis/operations/plot_force_and_position.py
@@ -19,7 +19,9 @@ def plot_force_data_and_position_data(overwrite_plots):
             for file in glob(os.path.join(path, "*.csv")):
                 data = pd.read_csv(file)
                 sensorfoot = data['sensorfoot'][1]
-                data_forces_and_pos = data[['force_x', 'force_y', 'force_z', 'FR_pos_x', 'FL_pos_x', 'HR_pos_x', 'HL_pos_x']]
+                data_forces_and_pos = data[['force_x', 'force_y', 'force_z', 'FR_pos_x', 'FL_pos_x', 'HR_pos_x', 'HL_pos_x',
+                                            'force_timestamp', 'imu_linacc_y', 'imu_timestamp', 'FR_timestamp', 'FL_timestamp',
+                                            'HR_timestamp', 'HL_timestamp']]
                 print("data_forces_and_pos: ", data_forces_and_pos.head())
 
                 x_forces = range(data_forces_and_pos['force_x'].count())
@@ -37,32 +39,42 @@ def plot_force_data_and_position_data(overwrite_plots):
                 sn.set_style('whitegrid')
 
                 scalefactor = 500
-                FR_pos_x = data_forces_and_pos[f'FR_pos_x'].dropna()
+                FR_pos_x = data_forces_and_pos['FR_pos_x']
                 FR_pos_x = [y * scalefactor for y in FR_pos_x]
                 FR_pos_x = FR_pos_x[0:len(min_x_pos)]
-                FL_pos_x = data_forces_and_pos[f'FL_pos_x'].dropna()
+                FL_pos_x = data_forces_and_pos['FL_pos_x']
                 FL_pos_x = [y * scalefactor for y in FL_pos_x]
                 FL_pos_x = FL_pos_x[0:len(min_x_pos)]
-                HR_pos_x = data_forces_and_pos[f'HR_pos_x'].dropna()
-                HR_pos_x = [y * -1*scalefactor for y in HR_pos_x]
+                HR_pos_x = data_forces_and_pos['HR_pos_x']
+                HR_pos_x = [y*scalefactor for y in HR_pos_x]
                 HR_pos_x = HR_pos_x[0:len(min_x_pos)]
-                HL_pos_x = data_forces_and_pos[f'HL_pos_x'].dropna()
-                HL_pos_x = [y * -1*scalefactor for y in HL_pos_x]
+                HL_pos_x = data_forces_and_pos['HL_pos_x']
+                HL_pos_x = [y*scalefactor for y in HL_pos_x]
                 HL_pos_x = HL_pos_x[0:len(min_x_pos)]
+                shift_value = abs(np.mean(FR_pos_x[:10])) + abs(np.mean(HR_pos_x[:10]))
+                HR_pos_x = [y + shift_value for y in HR_pos_x]
+                HL_pos_x = [y + shift_value for y in HL_pos_x]
+                imu_y = data_forces_and_pos['imu_linacc_y']
+                imu_y = [y + np.mean(FR_pos_x[:10]) for y in imu_y]
 
                 fig, ax = plt.subplots()
-                ax2 = ax.twiny()
-                sn.lineplot(x = x_forces, y = data_forces_and_pos['force_x'], data=data_forces_and_pos, ax=ax, color=colours[0], label=f'{sensorfoot}_force_x')
-                sn.lineplot(x = x_forces, y = data_forces_and_pos['force_y'], data=data_forces_and_pos, ax=ax, color=colours[1], label=f'{sensorfoot}_force_y')
-                sn.lineplot(x = x_forces, y = data_forces_and_pos['force_z'], data=data_forces_and_pos, ax=ax, color=colours[2], label=f'{sensorfoot}_force_z')
+                #ax2 = ax.twiny()
+                # plot imu data:
+                sn.lineplot(x = data_forces_and_pos['imu_timestamp'], y = imu_y, color='black', alpha=0.6, linewidth = 1, dashes=(5,5), label='imu linear acc. y')
 
-                sn.lineplot(x = min_x_pos, y = FR_pos_x, data=data_forces_and_pos, ax=ax2, palette=[colours[3]], label='FR', dashes=[(2,2)], linewidth=1)
-                sn.lineplot(x = min_x_pos, y = FL_pos_x, data=data_forces_and_pos, ax=ax2, palette=[colours[3]], label='FL', dashes=[(2,2)], linewidth=1)
-                sn.lineplot(x = min_x_pos, y = HR_pos_x, data=data_forces_and_pos, ax=ax2, palette=[colours[3]], label='HR', dashes=[(2,2)], linewidth=1)
-                sn.lineplot(x = min_x_pos, y = HL_pos_x, data=data_forces_and_pos, ax=ax2, palette=[colours[3]], label='HL', dashes=[(2,2)], linewidth=1)
+                # plot forces:
+                sn.lineplot(x = data_forces_and_pos['force_timestamp'], y = data_forces_and_pos['force_x'], ax=ax, color=colours[0], label=f'{sensorfoot}_force_x')
+                sn.lineplot(x = data_forces_and_pos['force_timestamp'], y = data_forces_and_pos['force_y'], ax=ax, color=colours[1], label=f'{sensorfoot}_force_y')
+                sn.lineplot(x = data_forces_and_pos['force_timestamp'], y = data_forces_and_pos['force_z'], ax=ax, color=colours[2], label=f'{sensorfoot}_force_z')
 
-                ax.set_xlabel("force timesteps")
-                ax2.set_xlabel("position timesteps")
+                # plot foot position data:
+                sn.lineplot(x = data_forces_and_pos['FR_timestamp'], y = FR_pos_x, data=data_forces_and_pos, label='FR', dashes=[(2,2)], linewidth=1)
+                sn.lineplot(x = data_forces_and_pos['FL_timestamp'], y = FL_pos_x, data=data_forces_and_pos, label='FL', dashes=[(2,2)], linewidth=1)
+                sn.lineplot(x = data_forces_and_pos['HL_timestamp'], y = HL_pos_x, data=data_forces_and_pos, label='HL', dashes=[(2,2)], linewidth=1)
+                sn.lineplot(x = data_forces_and_pos['HR_timestamp'], y = HR_pos_x, data=data_forces_and_pos, label='HR', dashes=[(2,2)], linewidth=1)
+
+                ax.set_xlabel("rosbag timesteps")
+                #ax2.set_xlabel("position timesteps")
                 plt.ylabel(f'{run_number}_forces')
 
                 fig1 = plt.gcf()
